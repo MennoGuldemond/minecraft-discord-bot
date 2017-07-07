5d2f81f574651a09e2598d73b5922c05735094a6
Menno Guldemond <mennoguldemond@live.nl>
2017-07-07 11:21:02 +0200

    Seperate code files (without the node_modules)

---
+++ Config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    Config: function(){
+        this.domain = 'www.myserver.com';
+        this.port = 25565;
+        this.botToken = '';
+
+        return this;
+    }
+
+};
